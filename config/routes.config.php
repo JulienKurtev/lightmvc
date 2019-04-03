40bfb9e2932aa18ff05d30d36a41bf5a57572cfe
JulienKurtev <julienemilovkurtev@gmail.com>
2019-04-04 02:55:32 +0300

    Add the right code

--- config/routes.config.php
+++ config/routes.config.php
@@ -41,4 +41,9 @@ $baseConfig['routes'] = [
         '/login[/{action}]',
         'login',
     ],
+    7 => [
+        'GET',
+        '/logout[/{action}]',
+        'logout',
+    ],
 ];
--- controllers/Application/Services/SessionService.php
+++ controllers/Application/Services/SessionService.php
@@ -1,223 +1,109 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: z_kurtev
- * Date: 2019-03-23
- * Time: 12:42 PM
- */
 
 namespace Application\Services;
 
 use Application\Models\Entity\Users;
-use Application\Models\Traits\DoctrineTrait;
 use Application\Models\Repository\UsersRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Mapping\ClassMetadata;
 
 class SessionService
 {
-    // Initialize application business and frontend messages.
-    protected $errorMessage = 0;
-    protected $userMessage = 'Please sign in';
-    protected $session="";
+    protected $users;
 
+    protected $usersRepository;
+
+    protected $em;
 
     // Set flags.
-    protected $loginCheck = FALSE;
-    protected $validSession = FALSE;
-    protected $postLoginForm = TRUE;
+    protected $authenticated = false;
 
-    /**
-     * @return int
-     */
-    public function getErrorMessage()
+    protected $validSession = false;
+
+    public function __construct(Users $users, EntityManager $em)
     {
-        return $this->errorMessage;
+        $this->users = $users;
+
+        $this->em = $em;
+
+        $this->usersRepository = new UsersRepository(
+            $this->em,
+            new ClassMetadata(Users::class)
+        );
+
+        $this->checkSession();
     }
 
-    /**
-     * @return string
-     */
-    public function getUserMessage()
+    public function checkLogin($username, $password)
     {
-        return $this->userMessage;
+        // Check credentials.
+        $results = $this->usersRepository->findByUsername($username);
+
+        if (password_verify($password, $results[0]->getPassword())) {
+            if ($this->validSession === false) {
+                $this->validSession = $this->session_secure_init();
+            }
+
+            if ($this->validSession === true) {
+                //  Check for cookie tampering.
+                if (isset($_SESSION['LOGGEDIN'])) {
+                    $this->validSession = $this->session_obliterate();
+                } else {
+                    setcookie('loggedin', true, time()+ 4200, '/');
+                    $_SESSION['LOGGEDIN'] = true;
+                    $_SESSION['REMOTE_USER'] = $username;
+                    $this->authenticated = true;
+
+                    return true;
+                }
+            } else {
+                $this->validSession = $this->session_obliterate();
+
+                return false;
+            }
+        } else {
+            $this->validSession = $this->session_obliterate();
+
+            return false;
+        }
     }
 
-    public function getPostLoginForm() {
-        return $this->postLoginForm;
-    }
-
-    public function loginVerification() {
-
+    public function checkSession()
+    {
         // Check if user is already logged in.
         if (isset($_COOKIE['loggedin'])) {
-
-            if ($this->validSession === FALSE) {
-
+            if ($this->validSession === false) {
                 $this->validSession = $this->session_secure_init();
             }
 
             //  Check for cookie tampering.
-            if ($this->validSession === TRUE && isset($_SESSION['LOGGEDIN'])) {
-                $this->postLoginForm = FALSE;
-            } else {
+            if ($this->validSession !== true && !isset($_SESSION['LOGGEDIN'])) {
                 $this->validSession = $this->session_obliterate();
-                $this->errorMessage = 3;
-                $this->postLoginForm = TRUE;
             }
 
             // Cookie login check done.
-            $this->loginCheck = TRUE;
+            $this->authenticated = true;
         }
 
-        if (isset($_POST['submit'])
-            && !empty($_POST['username'])
-            && !empty($_POST['password'])
-
-            ) {
-
-            // receive all input values from the form
-            // DANGER! - values must be filtered, validated and sanitized!
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-
-
-            // form validation: ensure that the form is correctly filled ...
-            // by adding (array_push()) corresponding error unto $errors array
-            if (empty($name)) {
-                $this->userMessage = '<span style="color: firebrick;text-align:center;">Username is required</span>';
-            }
-            if (empty($password)) {
-                $this->userMessage = '<span style="color: firebrick;text-align:center;">Password is required</span>';
-            }
-
-
-
-
-            $this->userMessage = isset($errorMessage) ? '<span style="color: firebrick;text-align:center;">'. $errorMessage .'</span>' : '';
-
-            return;
-        }
-
-        // Login verification.
-        if (isset($_POST['submit'])
-            && $_POST['submit'] == 1
-            && !empty($_POST['username'])
-            && !empty($_POST['password'])) {
-
-            if ($this->validSession === FALSE) {
-
-                $this->validSession = $this->session_secure_init();
-            }
-
-            $username = (string) $_POST['username'];
-            $password = (string) $_POST['password'];
-
-            if (!ctype_alpha($username)) {
-
-                $username = preg_replace("/[^a-zA-Z]+/", "", $username);
-            }
-
-            if (strlen($username) > 40) {
-
-                $username = substr($username, 0, 39);
-            }
-
-            $password = preg_replace("/[^_a-zA-Z0-9]+/", "", $password);
-
-            if (strlen($password) > 40) {
-
-                $password = substr($password, 0, 39);
-            }
-
-            // Check credentials.
-            if ($this->loginCheck) {
-
-                if ($this->validSession === TRUE) {
-
-                    //  Check for cookie tampering.
-                    if (isset($_SESSION['LOGGEDIN'])) {
-
-                        $this->validSession = $this->session_obliterate();
-                        $this->errorMessage = 3;
-                        $this->postLoginForm = TRUE;
-
-                    } else {
-
-                        setcookie('loggedin', TRUE, time()+ 4200, '/');
-                        $_SESSION['LOGGEDIN'] = TRUE;
-                        $_SESSION['REMOTE_USER'] = $username;
-                        $this->postLoginForm = FALSE;
-                        $this->session =  $_SESSION['REMOTE_USER'];
-
-                    }
-
-                } else {
-
-                    $this->validSession = $this->session_obliterate();
-                    $this->errorMessage = 3;
-                    $this->postLoginForm = TRUE;
-                }
-
-            } else {
-
-                $this->validSession = $this->session_obliterate();
-                $this->errorMessage = 1;
-                $this->postLoginForm = TRUE;
-            }
-
-            // Username-password login check done.
-            $this->loginCheck = TRUE;
-        }
-
-        // Intercept logout POST.
-        if (isset($_POST['logout'])) {
-
-            if ($this->validSession === FALSE) {
-
-                $this->session_secure_init();
-            }
-
+        // Intercept invalid sessions.
+        if ($this->authenticated === true && $this->validSession === false) {
+            $this->validSession = $this->session_secure_init();
             $this->validSession = $this->session_obliterate();
-            $this->errorMessage = 2;
-            $this->postLoginForm = TRUE;
-        }
-
-        // Intercept invalid sessions and redirect to login page.
-        if ($this->loginCheck === TRUE && $this->validSession === FALSE && $this->errorMessage === 0) {
-
-            if ($this->validSession === FALSE) {
-
-                $this->validSession = $this->session_secure_init();
-                $this->validSession = $this->session_obliterate();
-            }
-
-            $this->errorMessage = 3;
-            $this->postLoginForm = TRUE;
-        }
-
-        // Prepare view output.
-        if ($this->postLoginForm === TRUE) {
-
-            switch ($this->errorMessage) {
-                case 0:
-                    $this->userMessage = 'Please sign in';
-                    break;
-                case 1:
-                    $this->userMessage = 'Wrong credentials!  <a href="/login">Try again</a>.';
-
-                    break;
-                case 2:
-                    $this->userMessage = 'You are logged out!  <a href="/login">You can login again</a>.';
-                    break;
-                case 3:
-                    $this->userMessage = 'Invalid session <a href="/login">Please login again</a>.';
-                    break;
-            }
+            $this->authenticated = false;
         }
     }
 
-    public function session_obliterate()
+    public function logout()
+    {
+        if ($this->validSession === false) {
+            $this->session_secure_init();
+        }
+
+        $this->validSession = $this->session_obliterate();
+        $this->authenticated = false;
+    }
+
+    protected function session_obliterate()
     {
 
         $_SESSION = array();
@@ -225,64 +111,54 @@ class SessionService
         setcookie('loggedin', '', time() - 3600, '/');
         session_destroy();   // Destroy session data in storage.
         session_unset();     // Unset $_SESSION variable for the runtime.
-        $this->validSession = FALSE;
+        $this->validSession = false;
         return $this->validSession;
 
     }
 
-    public function session_secure_init()
+    protected function session_secure_init()
     {
         session_set_cookie_params(4200);
 
-        $this->validSession = TRUE;
+        $this->validSession = true;
 
         if (!defined('OURUNIQUEKEY')) {
-
             define('OURUNIQUEKEY', 'phpi');
-
         }
 
         // Avoid session prediction.
         $sessionname = OURUNIQUEKEY;
 
         if (session_name() != $sessionname) {
-
             session_name($sessionname);
-
         } else {
-
             session_name();
-
         }
 
         // Start session.
         session_start();
 
         if ((!isset($_COOKIE['loggedin']) && isset($_SESSION['LOGGEDIN']))
-            ^ (isset($_COOKIE['loggedin']) && !isset($_SESSION['LOGGEDIN']))) {
+            ^ (isset($_COOKIE['loggedin']) && !isset($_SESSION['LOGGEDIN']))
+        ) {
+            $this->validSession = false;
+        }
 
-            $this->validSession = FALSE;    }
-
-        if ($this->validSession == TRUE) {
-
+        if ($this->validSession == true) {
             // Avoid session fixation.
             if (!isset($_SESSION['INITIATED'])) {
-
                 session_regenerate_id();
                 $_SESSION['INITIATED'] = TRUE;
             }
 
             if (!isset($_SESSION['CREATED'])) {
-
                 $_SESSION['CREATED'] = time();
             }
 
             if (time() - $_SESSION['CREATED'] > 3600) {
-
                 // Session started more than 60 minutes ago.
                 session_regenerate_id();    // Change session ID for the current session an invalidate old session ID.
                 $_SESSION['CREATED'] = time();  // Update creation time.
-
             }
 
             // Avoid session hijacking.
@@ -291,29 +167,39 @@ class SessionService
             $useragent .= OURUNIQUEKEY;
 
             if (isset($_SESSION['HTTP_USER_AGENT'])) {
-
                 if ($_SESSION['HTTP_USER_AGENT'] != md5($useragent)) {
-
-                    $this->validSession = FALSE;
+                    $this->validSession = false;
                 }
-
             } else {
-
                 $_SESSION['HTTP_USER_AGENT'] = md5($useragent);
-
             }
 
             // Avoid session fixation in case of an inactive session.
-            if ($this->validSession == TRUE && isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY']) > 3600) {
-
+            if ($this->validSession === true && isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY']) > 3600) {
                 // Last request was more than 60 minutes ago.
-                $this->validSession = FALSE;
-
+                $this->validSession = false;
             } else {
-
                 $_SESSION['LAST_ACTIVITY'] = time(); // Update last activity timestamp.
             }
+
         }
+
+        return $this->validSession;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAuthenticated(): bool
+    {
+        return $this->authenticated;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValidSession(): bool
+    {
         return $this->validSession;
     }
 }
